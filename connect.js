37d9b578ff3a91c33b5cc3b76bb98af88e637795
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-03-25 13:49:43 -0500

    conectar con base de datos

--- connect.js
+++ connect.js
@@ -1,10 +1,20 @@
-const config = require('./config');
+const { MongoClient } = require('mongodb');
+const config = require("./config");
 
 // eslint-disable-next-line no-unused-vars
 const { dbUrl } = config;
 
+const client = new MongoClient(config.dbUrl);
+
 async function connect() {
-  // TODO: Database Connection
+  try {
+    await client.connect();
+    const db = client.db('burgerqueen'); // Reemplaza <NOMBRE_DB> por el nombre del db
+    console.log('conectado');
+    return db;
+  } catch (error) {
+    console.error(error);//
+  }
 }
 
 module.exports = { connect };
