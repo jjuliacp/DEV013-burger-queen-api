471f78078a7b2f2334c6ac7c18fbf2d6a106f0ae
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-04-19 09:52:55 -0500

    modificando variables de entorno para vercel

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb+srv://BurgerApi:jpYL6zpW0MhViGBO@cluster0.xmx09gn.mongodb.net/BurgerQueenApi?retryWrites=true&w=majority&appName=Cluster0';
+exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://127.0.0.1:27017/burguer_queenApi';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
